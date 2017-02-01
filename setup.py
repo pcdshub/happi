a7b8a7d3d951535f722d42191e7e35dd0cfa826c
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-02-01 00:33:14 +0000

    Added a setup.py file, looks like it works
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/package/trunk/happi@27568 ececa60f-9f63-0410-97c7-ea05888eb260

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import (setup, find_packages)
+
+
+setup(name    = 'happi',
+      version = '0.0.1',
+      license = 'BSD',
+      author  = 'SLAC National Accelerator Laboratory',
+
+      packages    = find_packages(),
+      description = 'Happi Database Access for LCLS Beamline Devices',
+
+    )
