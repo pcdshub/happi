c4bc2f30db8e934e3c9ddbce51c59f841c6b9077
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-03-20 10:34:03 -0700

    Explicit import of EntryInfo in module

--- happi/__init__.py
+++ happi/__init__.py
@@ -1,12 +1,11 @@
 import logging
 
+
+from .device import Device, EntryInfo
+from .client import Client
+
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
-
-#Devices
-from .device     import Device
-from .client     import Client
-
 
 from ._version import get_versions
 __version__ = get_versions()['version']
