0f9481a653e1883b953e7bf835260e2c743f5290
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-08-23 17:10:03 -0700

    TST: Test all client operations with both existing backends

--- happi/tests/test_client.py
+++ happi/tests/test_client.py
@@ -16,18 +16,30 @@ import pytest
 from happi            import Device, Client
 from happi.containers import GateValve
 from happi.errors     import *
+from .conftest import mockclient, mockjsonclient, device_info
 
-def test_find_document(mockclient, device_info):
-    doc = mockclient.find_document(**device_info)
+
+# Have to do this the sloppy way until you can use fixtures as 
+# pytest parameters see https://github.com/pytest-dev/pytest/issues/349
+@pytest.fixture(scope='function', params=[mockclient, mockjsonclient],
+                ids=['mongo', 'json'])
+def mc(request):
+    #Give the instantiated client
+    client = request.param(device_info())
+    yield client
+    if getattr(client.backend, 'path', None):
+        os.remove(client.backend.path)
+
+def test_find_document(mc, device_info):
+    doc = mc.find_document(**device_info)
     assert doc.pop('prefix')     == device_info['prefix']
     assert doc.pop('name')    == device_info['name']
     assert doc.pop('z')        == device_info['z']
     assert doc.pop('beamline') == device_info['beamline']
 
-
     #Remove id and check
     prefix = device_info.pop('prefix')
-    doc  = mockclient.find_document(**device_info)
+    doc  = mc.find_document(**device_info)
     assert doc.pop('prefix')     == prefix
     assert doc.pop('name')    == device_info['name']
     assert doc.pop('z')        == device_info['z']
@@ -35,10 +47,10 @@ def test_find_document(mockclient, device_info):
 
     #Not available
     with pytest.raises(SearchError):
-        doc = mockclient.find_document(prefix='Does not Exist')
+        doc = mc.find_document(prefix='Does not Exist')
 
-def test_create_device(mockclient, device_info):
-    device = mockclient.create_device(Device, **device_info)
+def test_create_device(mc, device_info):
+    device = mc.create_device(Device, **device_info)
     assert device.prefix     == device_info['prefix']
     assert device.name    == device_info['name']
     assert device.z        == device_info['z']
@@ -46,10 +58,10 @@ def test_create_device(mockclient, device_info):
 
     #Invalid Entry
     with pytest.raises(TypeError):
-        mockclient.create_device(int)
+        mc.create_device(int)
 
-def test_create_valve(mockclient, valve_info):
-    device = mockclient.create_device(GateValve, **valve_info)
+def test_create_valve(mc, valve_info):
+    device = mc.create_device(GateValve, **valve_info)
     assert isinstance(device, GateValve)
     assert device.prefix     == valve_info['prefix']
     assert device.name    == valve_info['name']
@@ -57,7 +69,7 @@ def test_create_valve(mockclient, valve_info):
     assert device.beamline == valve_info['beamline']
 
     #Specify string as class
-    device = mockclient.create_device('GateValve', **valve_info)
+    device = mc.create_device('GateValve', **valve_info)
     assert isinstance(device, GateValve)
     assert device.prefix     == valve_info['prefix']
     assert device.name    == valve_info['name']
@@ -66,19 +78,19 @@ def test_create_valve(mockclient, valve_info):
 
     #Save
     device.save()
-    loaded_device = mockclient.load_device(**valve_info)
+    loaded_device = mc.load_device(**valve_info)
     assert loaded_device.prefix     == valve_info['prefix']
     assert loaded_device.name    == valve_info['name']
     assert loaded_device.z        == valve_info['z']
     assert loaded_device.beamline == valve_info['beamline']
 
 
-def test_all_devices(mockclient, device):
-    assert mockclient.all_devices == [device]
+def test_all_devices(mc, device):
+    assert mc.all_devices == [device]
 
-def test_add_device(mockclient, valve, device, valve_info):
-    mockclient.add_device(valve)
-    doc = mockclient._collection.find_one(valve_info)
+def test_add_device(mc, valve, device, valve_info):
+    mc.add_device(valve)
+    doc = mc.backend.find(multiples=False, **valve_info)
     assert valve.prefix     == doc['prefix']
     assert valve.name    == doc['name']
     assert valve.z        == doc['z']
@@ -86,17 +98,17 @@ def test_add_device(mockclient, valve, device, valve_info):
 
     #No duplicates
     with pytest.raises(DuplicateError):
-        mockclient.add_device(device)
-    
+        mc.add_device(device)
+
     #No incompletes
     d = Device()
     with pytest.raises(EntryError):
-        mockclient.add_device(d)
+        mc.add_device(d)
 
 
-def test_add_and_load_device(mockclient, valve, valve_info):
-    mockclient.add_device(valve)
-    loaded_device = mockclient.load_device(**valve_info)
+def test_add_and_load_device(mc, valve, valve_info):
+    mc.add_device(valve)
+    loaded_device = mc.load_device(**valve_info)
     assert loaded_device.prefix     == valve.prefix
     assert loaded_device.name    == valve.name
     assert loaded_device.z        == valve.z
@@ -104,8 +116,8 @@ def test_add_and_load_device(mockclient, valve, valve_info):
 
 
 
-def test_load_device(mockclient, device_info):
-    device = mockclient.load_device(**device_info)
+def test_load_device(mc, device_info):
+    device = mc.load_device(**device_info)
     assert isinstance(device, Device)
     assert device.prefix     == device_info['prefix']
     assert device.name    == device_info['name']
@@ -116,7 +128,7 @@ def test_load_device(mockclient, device_info):
     device.stand = 'DG3'
     device.save()
 
-    loaded_device = mockclient.load_device(**device_info)
+    loaded_device = mc.load_device(**device_info)
     assert loaded_device.prefix     == device_info['prefix']
     assert loaded_device.name    == device_info['name']
     assert loaded_device.z        == device_info['z']
@@ -125,22 +137,22 @@ def test_load_device(mockclient, device_info):
 
     #Bad load
     bad = {'a':'b'}
-    mockclient._collection.insert_one(bad)
+    mc.backend.save('a', bad, insert=True)
     with pytest.raises(EntryError):
-        mockclient.load_device(**bad)
+        mc.load_device(**bad)
 
 
-def test_validate(mockclient):
+def test_validate(mc):
     #No bad devices
-    assert mockclient.validate() == list()
+    assert mc.validate() == list()
     #A single bad device
-    mockclient._collection.insert_one({'_id':'bad'})
-    assert mockclient.validate() == ['bad']
+    mc.backend.save('_id', {'prefix':'bad'}, insert=True)
+    assert mc.validate() == ['bad']
 
 
-def test_search(mockclient, device, valve, device_info, valve_info):
-    mockclient.add_device(valve)
-    res = mockclient.search(name=device_info['name'])
+def test_search(mc, device, valve, device_info, valve_info):
+    mc.add_device(valve)
+    res = mc.search(name=device_info['name'])
     #Single search return
     assert len(res) == 1
     loaded_device = res[0]
@@ -150,10 +162,10 @@ def test_search(mockclient, device, valve, device_info, valve_info):
     assert loaded_device.beamline == device_info['beamline']
 
     #No results
-    assert mockclient.search(name='not') == None
+    assert mc.search(name='not') == None
 
     #returned as dict
-    res = mockclient.search(as_dict=True, **device_info)
+    res = mc.search(as_dict=True, **device_info)
     loaded_device = res[0]
     assert loaded_device['prefix']     == device_info['prefix']
     assert loaded_device['name']    == device_info['name']
@@ -161,55 +173,49 @@ def test_search(mockclient, device, valve, device_info, valve_info):
     assert loaded_device['beamline'] == device_info['beamline']
 
     #Search between two points
-    res = mockclient.search(start=0, end=500)
+    res = mc.search(start=0, end=500)
     assert len(res) == 2
     loaded_device = res[0]
-    assert loaded_device.prefix     == device_info['prefix']
-    assert loaded_device.name    == device_info['name']
-    assert loaded_device.z        == device_info['z']
-    assert loaded_device.beamline == device_info['beamline']
 
     #Search between two points, nothing found
-    res = mockclient.search(start=10000, end=500000)
+    res = mc.search(start=10000, end=500000)
     assert res == None
 
     #Search without an endpoint
-    res = mockclient.search(start=0)
+    res = mc.search(start=0)
     assert len(res) == 2
     loaded_device = res[1]
-    assert loaded_device.prefix     == valve_info['prefix']
-    assert loaded_device.name    == valve_info['name']
-    assert loaded_device.z        == valve_info['z']
-    assert loaded_device.beamline == valve_info['beamline']
 
     #Search invalid range
     with pytest.raises(ValueError):
-        mockclient.search(start=1000,end=5)
+        mc.search(start=1000,end=5)
     
     #Search off keyword
-    res = mockclient.search(beamline='LCLS')
+    res = mc.search(beamline='LCLS')
     assert len(res) == 2
 
 
-def test_remove_device(mockclient,device, valve, device_info):
-    mockclient.remove_device(device)
-    assert mockclient._collection.find_one(device_info) == None
+def test_remove_device(mc,device, valve, device_info):
+    mc.remove_device(device)
+    assert mc.backend.find(**device_info) == []
 
     #Invalid Device
     with pytest.raises(ValueError):
-        mockclient.remove_device(5)
+        mc.remove_device(5)
     
     #Valve not in dictionary
     with pytest.raises(SearchError):
-        mockclient.remove_device(valve)
+        mc.remove_device(valve)
 
-def test_export(mockclient, valve):
+def test_export(mc, valve):
     #Setup client with both devices
-    mockclient.add_device(valve)
+    mc.add_device(valve)
     fd, temp_path = tempfile.mkstemp()
 
-    mockclient.export(open(temp_path, 'w+'), sep=',', attrs=['name','prefix'])
-    assert open(temp_path,'r').read() == "alias,BASE:PV\nname,BASE:VGC:PV\n"
+    mc.export(open(temp_path, 'w+'), sep=',', attrs=['name','prefix'])
+    exp = open(temp_path,'r').read()
+    assert "alias,BASE:PV"    in exp
+    assert "name,BASE:VGC:PV" in exp
    
     #Cleanup
     os.remove(temp_path)
