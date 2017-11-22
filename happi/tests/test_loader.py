e5f8444f81b099343a2bb1de19c3db04c3123278
Teddy Rendahl <trendahl@slac.stanford.edu>
2017-11-22 11:27:42 -0800

    TST: Add test for sequential device loading

--- happi/tests/test_loader.py
+++ happi/tests/test_loader.py
@@ -11,7 +11,12 @@
 # Module #
 ##########
 from happi import Device, EntryInfo
-from happi.loader import fill_template, from_container
+from happi.loader import fill_template, from_container, load_devices
+from happi.utils import create_alias
+
+
+class TimeDevice(Device):
+    days = EntryInfo("Number of days", enforce=int)
 
 
 def test_fill_template(device):
@@ -25,8 +30,6 @@ def test_fill_template(device):
 
 
 def test_from_container():
-    class TimeDevice(Device):
-        days = EntryInfo("Number of days", enforce=int)
     # Create a datetime device
     d = TimeDevice(name='Test', prefix='Tst:This', beamline='TST',
                    device_class='datetime.timedelta', args=list(), days=10,
@@ -35,3 +38,26 @@ def test_from_container():
     # Now import datetime and check that we constructed ours correctly
     import datetime
     assert td == datetime.timedelta(days=10, seconds=30)
+
+
+def test_load_devices():
+    # Create a bunch of devices to load
+    devs = [TimeDevice(name='Test 1', prefix='Tst1:This', beamline='TST',
+                       device_class='datetime.timedelta', args=list(), days=10,
+                       kwargs={'days': '{{days}}', 'seconds': 30}),
+            TimeDevice(name='Test 2', prefix='Tst2:This', beamline='TST',
+                       device_class='datetime.timedelta', args=list(), days=10,
+                       kwargs={'days': '{{days}}', 'seconds': 30}),
+            TimeDevice(name='Test 3', prefix='Tst3:This', beamline='TST',
+                       device_class='datetime.timedelta', args=list(), days=10,
+                       kwargs={'days': '{{days}}', 'seconds': 30}),
+            Device(name='Bad', prefix='Not:Here', beamline='BAD',
+                   device_class='non.existant')]
+    # Load our devices
+    space = load_devices(*devs, pprint=True)
+    # Check all our devices are there
+    assert all([create_alias(dev.name) in space.__dict__ for dev in devs])
+    # Devices were loading properly or exceptions were stored
+    import datetime
+    assert space.test_1 == datetime.timedelta(days=10, seconds=30)
+    assert isinstance(space.bad, ImportError)
