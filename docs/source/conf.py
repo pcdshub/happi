10a579281eaf027e2206c5df87fb4be65cc7f891
teddyrendahl <trendahl@slac.stanford.edu>
2018-10-16 15:56:27 -0700

    DEV: Remove need to call autosummary externally

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,6 +43,8 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.autosectionlabel'
              ]
 
+autosummary_generate = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
