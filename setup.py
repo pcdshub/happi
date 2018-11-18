e9fffd1669bc4cf5803fc89bd3791bec6f694b66
Thomas A Caswell <tcaswell@bnl.gov>
2018-11-18 14:56:45 +1100

    DEV: use requirements.txt to fill in install_requires

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 import versioneer
 from setuptools import (setup, find_packages)
 
+with open('requirements.txt') as f:
+    requirements = f.read().split()
+
+
 setup(name     = 'happi',
       version  = versioneer.get_version(),
       cmdclass = versioneer.get_cmdclass(),
@@ -9,5 +13,5 @@ setup(name     = 'happi',
 
       packages    = find_packages(),
       description = 'Happi Database Access for LCLS Beamline Devices',
-
+      install_requires=requirements,
     )
