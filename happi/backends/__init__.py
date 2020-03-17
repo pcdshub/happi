f6e877535e167262d314173522a6d94d55b1ea40
K Lauer <klauer@slac.stanford.edu>
2020-03-17 14:34:58 -0700

    REF: allow access to all backends in happi.backends.BACKENDS dict

--- happi/backends/__init__.py
+++ happi/backends/__init__.py
@@ -1,24 +1,42 @@
-__all__ = ['backend']
+__all__ = ['backend', 'BACKENDS', 'DEFAULT_BACKEND']
 import os
-# Check to see if the user has specified a specific backend
-# as an environment variable. Import this as the standard
-# backend for other places in the module. A user can always
-# override this by explicitly importing the backend
-_backend = os.environ.get("HAPPI_BACKEND", 'json').lower()
-
 
 def _get_backend(backend):
     if backend == 'mongodb':
         from .mongo_db import MongoBackend
         return MongoBackend
-    elif backend == 'json':
+    if backend == 'json':
         from .json_db import JSONBackend
         return JSONBackend
-    else:
-        raise ImportError("Improper specification of happi backend."
-                          "Check `$HAPPI_BACKEND` environment variable.")
+    raise ValueError(f'Unknown backend {backend!r}')
 
 
-backend = _get_backend(_backend)
+def _get_backends():
+    # A hook for entrypoints or something similar later
+    backends = {}
+    try:
+        backends['json'] = _get_backend('json')
+    except ImportError as ex:
+        logger.warning('JSON backend unavailable: %s', ex)
 
-del _backend
+    try:
+        backends['mongodb'] = _get_backend('mongodb')
+    except ImportError as ex:
+        logger.warning('MongoDB backend unavailable: %s', ex)
+
+    return backends
+
+
+BACKENDS = _get_backends()
+try:
+    # Check to see if the user has specified a specific backend as an
+    # environment variable. Import this as the standard backend for other
+    # places in the module. A user can always override this by explicitly
+    # importing the backend
+    DEFAULT_BACKEND = BACKENDS[os.environ.get("HAPPI_BACKEND", 'json').lower()]
+except KeyError:
+    raise ImportError("Improper specification of happi backend. "
+                      "Check the `$HAPPI_BACKEND` environment variable.")
+
+# back-compatibility
+backend = DEFAULT_BACKEND
