e27b90b1cf4bc0ec5dd0f94202f55f65b7d19163
Zachary Lentz <zlentz@slac.stanford.edu>
2020-03-20 09:20:24 -0700

    MNT: update top-level init imports

--- happi/__init__.py
+++ happi/__init__.py
@@ -1,10 +1,11 @@
 __all__ = ['Device', 'EntryInfo', 'Client', 'from_container',
-           'load_devices', 'cache', 'HappiItem']
+           'load_devices', 'cache', 'HappiItem', 'OphydItem']
 import logging
 
 from ._version import get_versions
 from .client import Client
-from .device import Device, EntryInfo, HappiItem
+from .device import Device
+from .item import EntryInfo, HappiItem, OphydItem
 from .loader import cache, from_container, load_devices
 
 __version__ = get_versions()['version']
