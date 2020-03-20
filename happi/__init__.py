d3609047cf3a1b87ad874876240cd9a871db567d
Zachary Lentz <zlentz@slac.stanford.edu>
2020-03-20 09:23:07 -0700

    MNT: remove logging setup from top level init

--- happi/__init__.py
+++ happi/__init__.py
@@ -1,7 +1,5 @@
 __all__ = ['Device', 'EntryInfo', 'Client', 'from_container',
            'load_devices', 'cache', 'HappiItem', 'OphydItem']
-import logging
-
 from ._version import get_versions
 from .client import Client
 from .device import Device
@@ -10,7 +8,3 @@ from .loader import cache, from_container, load_devices
 
 __version__ = get_versions()['version']
 del get_versions
-
-# Generic Logging Setup
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.NullHandler())
