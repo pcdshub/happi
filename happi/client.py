2ce39fd9991d126dd406d15fa2d634e1d2c3097a
Zachary Lentz <zlentz@slac.stanford.edu>
2020-06-02 16:07:22 -0700

    MNT: don't error out if one of many devices has a bad entry

--- happi/client.py
+++ happi/client.py
@@ -378,8 +378,14 @@ class Client(collections.abc.Mapping):
         Return search results to the user, optionally wrapping with a class
         '''
         wrap_cls = wrap_cls or self._results_wrap_class
-        return [wrap_cls(client=self, device=self.find_device(**info))
-                for info in items]
+        results = []
+        for info in items:
+            try:
+                result = wrap_cls(client=self, device=self.find_device(**info))
+                results.append(result)
+            except Exception:
+                logger.warning(f'Entry for {info["name"]} is malformed. Skipping.')
+        return results
 
     def search_range(self, key, start, end=None, **kwargs):
         """
