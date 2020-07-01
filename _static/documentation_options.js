5adcfe4a335b46d7ab626c7d63f5f142671d0cef
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-07-01 22:52:07 +0000

    Update docs after building Travis build 352 of pcdshub/happi
    The docs were built from the branch 'master' against the commit
    e5e50fa09cacaa98dff7b37a7239654e7e488316.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/pcdshub/happi/jobs/356595679.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy . --built-docs docs/build/html --deploy-branch-name gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.6.0+17.g7f3325a',
+    VERSION: '1.6.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
