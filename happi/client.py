55a83ad808a2062c63a22e4227d326db213ba9c3
cristinasewell <csewell@slac.stanford.edu>
2020-08-18 15:51:30 -0700

    MNT: move if block outside try/except KeyError

--- happi/client.py
+++ happi/client.py
@@ -606,24 +606,24 @@ class Client(collections.abc.Mapping):
 
         # Find id
         try:
-            # In case we want to update the name of an entry
-            # We want to add a new entry, and delete the old one
             _id = post[self._id_key]
-            if the_old_name and the_old_name != post[self._id_key]:
-                # Store information for the new entry
-                logger.info('Saving new entry %s ...', _id)
-                self.backend.save(_id, post, insert=True)
-                # Remove the information for the old entry
-                logger.info('Removing old entry %s ...', the_old_name)
-                self.backend.delete(the_old_name)
-            else:
-                # Store information
-                logger.info('Adding / Modifying information for %s ...', _id)
-                self.backend.save(_id, post, insert=insert)
         except KeyError:
             raise EntryError('HappiItem did not supply the proper information '
                              'to interface with the database, missing {}'
                              ''.format(self._id_key))
+        # In case we want to update the name of an entry
+        # We want to add a new entry, and delete the old one
+        if the_old_name and the_old_name != post[self._id_key]:
+            # Store information for the new entry
+            logger.info('Saving new entry %s ...', _id)
+            self.backend.save(_id, post, insert=True)
+            # Remove the information for the old entry
+            logger.info('Removing old entry %s ...', the_old_name)
+            self.backend.delete(the_old_name)
+        else:
+            # Store information
+            logger.info('Adding / Modifying information for %s ...', _id)
+            self.backend.save(_id, post, insert=insert)
         return _id
 
     @classmethod
