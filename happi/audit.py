80131d68fee6fff9ff78bca4f69a361520a4dbc2
cristinasewell <csewell@slac.stanford.edu>
2020-09-02 23:14:04 -0700

    WIP: validating mandatory info and some enforce info - but running into problems

--- happi/audit.py
+++ happi/audit.py
@@ -6,14 +6,17 @@ Happpi configuration file using the Client.find_config method,
 otherwiwe it will use the path specified after --file
 """
 from configparser import ConfigParser
-import json
 import logging
 import os
 import sys
 
-import happi
-
 from happi.client import Client
+
+# For back-compat to <py3.7
+try:
+    from re import Pattern
+except ImportError:
+    from re import _pattern_type as Pattern
 
 
 logger = logging.getLogger(__name__)
@@ -22,7 +25,7 @@ logger = logging.getLogger(__name__)
 class Command(object):
     """
     Bluprint for the command class
-    This will be useful if all the commands are inhering from it
+    This will be useful if all the commands are inheriting from it
     """
     def __init__(self, name, summary):
         self.name = name
@@ -38,6 +41,9 @@ class Command(object):
 
 
 class Audit(Command):
+    """
+    Audits the database for valid entries.
+    """
     def __init__(self):
         self.name = 'audit'
         self.help = "Inspect a database's entries"
@@ -84,26 +90,123 @@ class Audit(Command):
     def validate_file(self, file_path):
         """
         Tests whether a path is a regular file
+
+        Parameters
+        -----------
+        file_path: str
+            File path to be validate
+
+        Returns
+        -------
+        bool
+            To indicate whether the file is a valid regular file
         """
         return os.path.isfile(file_path)
 
+    # TODO this is probably not needed, it assumes only json database...
+    # but keep it here for reference for now
+    # def parse_database(self, database_path):
+    #     """
+    #     Goes through an entire database and parses
+    #     all the entries
+
+    #     Parameters
+    #     -----------
+    #     database_path: str
+    #         Path to the database to be validated
+    #     """
+    #     # check for an empty database???
+    #     # check for an invalid file
+    #     # we are assuming
+    #     registry = happi.containers.registry
+    #     with open(database_path) as f:
+    #         data = json.loads(f.read())
+    #         for key, value in data.items():
+    #             # here i have to also check if the type is
+    #             # correct, and if it has a type???
+    #             try:
+    #                 container = registry[value.get('type')]
+    #                 logger.info('Container %s', container)
+    #             except Exception as ex:
+    #                 logger.exception('Something went wrong with '
+    #                                  'getting the type of the item: %s', ex)
+    #             self.validate_item(database_path, container, value)
+
+    def validate_mandatory_info(self, item):
+        for info in item.entry_info:
+            # if it is not optional, it should probably never be None
+            if not info.optional and item.get(info.key) is None:
+                logger.info('Entry %s must not have None for '
+                            'a mandatory entry: %s', item, info)
+
+    def validate_enforce(self, item):
+        for info in item.entry_info:
+            value = item.get(info.key)
+            if isinstance(info.enforce, type):
+                if info.enforce == str:
+                    # check the values that are enforced to be strings
+                    # TODO - not working for all items, if i have a number
+                    # inserted into an attribute where it should be string
+                    # this will still consider it as string.... not what i want
+                    if (not isinstance(value, str) and (value is not None) and
+                       info.default is not None):
+                        logger.info('The %s did not match the enforced type '
+                                    '%s for the entry: %s. Current value: %s',
+                                    info, info.enforce, item, value)
+                if info.enforce == float:
+                    # check the values that are enforced to be floats
+                    # TODO - this is not going to work here, because
+                    # even if i have something like '343.43' for z value
+                    # where it has quotes and inserted as string basically
+                    # this will still work and not consider it a bad entry
+                    print(f'The ones that are floats: {value}')
+                    if (not isinstance(value, float)):
+                        logger.info('The %s did not match the enforced type '
+                                    '%s for the entry: %s. Current value: %s',
+                                    info, info.enforce, item, value)
+                if info.enforce == list:
+                    # check the values that are enforced to be lists
+                    pass
+                if info.enforce == dict:
+                    pass
+                    # check the values that are enforced to be dictionaries
+                if info.enforce == bool:
+                    pass
+                    # check the values that are enforced to be boolean
+
+            # check the values that are enforced with regex
+            elif isinstance(info.enforce, Pattern):
+                if not info.enforce.match(str(value)):
+                    logger.info('The %s did not match the enforced pattern '
+                                '%s for the entry: %s. Current value: %s',
+                                info, info.enforce, item, value)
+
     def parse_database(self, database_path):
         """
-        Goes through an entire database and validates
-        all the entries there
-        """
-        registry = happi.containers.registry
-        with open(database_path) as f:
-            data = json.loads(f.read())
-            for key, value in data.items():
-                # here i have to also check if the type is
-                # correct, and if it has a type???
-                container = registry[value.get('type')]
-                logger.debug('Container %s ', container)
+        Validates if an entry is a valid entry in the database
 
-    def validate_item(self, container, item_list):
-        try:
-            #  to validate
-            pass
-        except Exception as e:
-            logger.error(e)
+        Parameters
+        ----------
+        database_path: str
+            Path to the database to be validated
+
+        """
+        it = None
+        client = Client(path=database_path)
+        for item in client.backend.all_devices:
+            items = client.find_document(**item)
+            try:
+                # try to instantiate so you can have access to entry_info
+                it = client.create_device(items['type'], **items)
+                self.validate_mandatory_info(it)
+                self.validate_enforce(it)
+            except Exception:
+                # TODO handle these guys differently...
+                logger.info('Could not create a device.....')
+
+        # TODO if using the all_items I run
+        # into exceptions and can't look at all the devices
+        # items = client.all_items
+        # for item in items:
+        #   self.validate_mandatory_info(item)
+        #   self.validate_enforce(item)
