12228a01a3af8527fa12fa3a38c82a48a0dab3b4
cristinasewell <csewell@slac.stanford.edu>
2020-09-08 16:23:35 -0700

    WIP: add extras as argument for audit, and check module in PyPi

--- happi/audit.py
+++ happi/audit.py
@@ -9,6 +9,8 @@ from configparser import ConfigParser
 import logging
 import os
 import sys
+import subprocess
+import re
 from happi.client import Client
 from happi.loader import import_class, fill_template
 from happi.containers import registry
@@ -41,10 +43,17 @@ class Audit(Command):
     def __init__(self):
         self.name = 'audit'
         self.help = "Inspect a database's entries"
+        self._all_devices = set()
+        self._checked_devices = []
+        self._all_items = []
 
     def add_args(self, cmd_parser):
         cmd_parser.add_argument(
             "--file", help='Path to the json file (database) to be audited'
+        )
+        cmd_parser.add_argument(
+            "--extras", action='store_true', default=False,
+            help='Check specifically for extra attributes'
         )
 
     def run(self, args):
@@ -54,7 +63,10 @@ class Audit(Command):
         if args.file is not None:
             if self.validate_file(args.file):
                 logger.info('Using database file at %s ', args.file)
-                self.parse_database(args.file)
+                if args.extras:
+                    self.check_extra_attributes(args.file)
+                else:
+                    self.parse_database(args.file)
             else:
                 logger.error('Probably provided a wrong path or filename')
         else:
@@ -74,7 +86,10 @@ class Audit(Command):
                     return
                 if self.validate_file(database_path):
                     logger.info('Using database file at %s ', database_path)
-                    self.parse_database(database_path)
+                    if args.extras:
+                        self.check_extra_attributes(database_path)
+                    else:
+                        self.parse_database(database_path)
                 else:
                     logger.error('The database %s file path '
                                  'could not be validated', database_path)
@@ -137,12 +152,13 @@ class Audit(Command):
             logger.warning('Probably provided invalid argument: %s for %s, %s',
                            arg, item.name, e)
 
-    def validate_device_class(self, item):
+    def get_device_class(self, item):
         """
         Validates device_class field
         """
         device_class = item.get('device_class')
         device = item.get('name')
+
         if not device_class:
             logger.warning('Detected a None vlaue for %s. '
                            'The device_class cannot be None', device)
@@ -153,10 +169,32 @@ class Audit(Command):
                 logger.warning('Wrong device name format: %s for %s, %s',
                                device_class, device, e)
             else:
+                packages = device_class.rsplit('.')
+                package = packages[0]
+                self._all_devices.add(package)
+                self._all_items.append(item)
+
+    def validate_import_class(self):
+        """
+        Validate the device_class of an item
+        """
+        for item in self._all_items:
+            device_class = item.get('device_class')
+            dev_cls = device_class.rsplit('.')[0]
+            device = item.get('name')
+
+            if dev_cls in self._all_devices:
+                # has been tested agains PyPi and was found there
                 try:
                     import_class(device_class)
-                except ImportError as ex:
-                    logger.warning(ex)
+                except ImportError as e:
+                    logger.warning('For device: %s, %s. Either %s is '
+                                   'misspelled or %s is not part of the '
+                                   'python environment', device, e,
+                                   device_class, dev_cls)
+            elif dev_cls and dev_cls not in self._all_devices:
+                logger.warning('Provided wrong device/module name: %s. '
+                               'This module does not exist in PyPi', dev_cls)
 
     def validate_extra_attributes(self, item):
         """
@@ -174,10 +212,61 @@ class Audit(Command):
             logger.warning('Device %s has extra attributes %s',
                            item.name, diff)
 
+    def search_pip_package(self, package):
+        """
+        Checks to see if the package is on pypi
+
+        Parameters
+        -----------
+        package: str
+            Name of the package to check
+
+        Returns
+        --------
+            bool
+                To indicate if the package was found or not
+        """
+        process = subprocess.Popen('/bin/bash', stdin=subprocess.PIPE,
+                                   stdout=subprocess.PIPE, encoding='utf-8')
+        try:
+            arguments = ['pip', 'search', package]
+            command = ' '.join(arguments)
+
+            out, err = process.communicate(command)
+
+            # the output of 'pip search' will give all the fuzzy searches
+            # we only care if the exact package name is there
+            the_match = re.compile(package)
+            if re.match(the_match, out):
+                return True
+            else:
+                return False
+        except Exception as e:
+            logger.warning(e)
+            if process:
+                process.kill()
+
     def print_report_message(self, message):
         logger.info('')
         logger.info('--------- %s ---------', message)
         logger.info('')
+
+    def check_extra_attributes(self, database_path):
+        """
+        Checks the entries that have extra attributes
+
+        Parameters
+        ----------
+        database_path: str
+            Path to the database to be validated
+
+        """
+        client = Client(path=database_path)
+        items = client.all_items
+
+        self.print_report_message('EXTRA ATTRIBUTES')
+        for item in items:
+            self.validate_extra_attributes(item)
 
     def parse_database(self, database_path):
         """
@@ -193,24 +282,38 @@ class Audit(Command):
         items = client.all_items
 
         self.print_report_message('VALIDATING ENTRIES')
-        client.validate()
+        # this will fail to validate because pcdsdevices not imported yet....
+        # client.validate()
 
-        self.print_report_message('VALIDATING ARGS, KWARGS, EXTRA ATTRIBUTES')
-        for item in items:
-            self.validate_args(item)
-            self.validate_kwargs(item)
-            self.validate_extra_attributes(item)
-            pass
+        if items:
+            self.print_report_message('VALIDATING ARGS & KWARGS')
+            for item in items:
+                self.validate_args(item)
+                self.validate_kwargs(item)
+        else:
+            logger.error('Cannot run a set of tests becase the '
+                         'items could not be loaded.')
 
-        # TODO: what to do here???....
-        # for entr, item in zip(items, client.backend.all_devices):
-        #     #self.validate_enforce(entr, item)
-        #     pass
-
-        self.print_report_message('VALIDATING DEVICE CLASS')
         for item in client.backend.all_devices:
             it = client.find_document(**item)
-            self.validate_device_class(it)
+            self.get_device_class(it)
+
+        self.print_report_message('VALIDATE DEVICE MODULE EXISTS IN PYPI')
+        if self._all_devices:
+            temp_set = self._all_devices.copy()
+            for package in self._all_devices:
+                is_package_found = self.search_pip_package(package)
+                if is_package_found:
+                    logger.info('%s was found in PyPi', package)
+                else:
+                    # maybe a wrong device name was provided because
+                    # it does not exit
+                    logger.info('%s does not exit in PyPi', package)
+                    temp_set.remove(package)
+            self._all_devices = temp_set
+            # validate import_class
+            self.print_report_message('VALIDATING DEVICE CLASS')
+            self.validate_import_class()
 
         self.print_report_message('VALIDATING CONTAINER')
         for item in client.backend.all_devices:
