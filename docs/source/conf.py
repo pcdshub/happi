05295b8617e23bb3d2ab27f17dac6ef36008a11b
K Lauer <klauer@slac.stanford.edu>
2021-01-06 15:06:02 -0800

    DOC: down with the deck

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -166,7 +166,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'happideck', 'Happi Documentation',
+    (master_doc, 'happi', 'Happi Documentation',
      [author], 1)
 ]
 
