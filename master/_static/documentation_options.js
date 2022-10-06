7f3f5caae94f7b68044cc1b16c771185eefc2bd6
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2022-10-06 23:15:38 +0000

    Update docs after building Travis build 723 of pcdshub/happi
    The docs were built from the tag 'master' against the commit
    dc37705ab2ae93fc2f88bd67f5a771e7f80c8a28.
    
    The Travis build that generated this commit is at
    https://app.travis-ci.com/pcdshub/happi/jobs/584968847.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.8.13/bin/doctr deploy --build-tags master --deploy-branch-name gh-pages --command '"doctr-versions-menu"'

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.14.0+35.ga93837c',
+    VERSION: '1.14.0+45.gdc37705',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
