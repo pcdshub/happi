3b12e8c1449ad9d8163367db9e6da1e70859f0f1
tangkong <tangkongrobert@gmail.com>
2023-01-31 16:52:31 -0800

    BLD: migrate to setuptools-scm
    Performed by pcds-migration-tools SetuptoolsScmMigration

--- happi/__init__.py
+++ happi/__init__.py
@@ -1,3 +1,4 @@
+from .version import __version__  # noqa: F401
 __all__ = [
     "Client",
     "EntryInfo",
@@ -8,10 +9,6 @@ __all__ = [
     "from_container",
     "load_devices",
 ]
-from ._version import get_versions
 from .client import Client, SearchResult
 from .item import EntryInfo, HappiItem, OphydItem
 from .loader import cache, from_container, load_devices
-
-__version__ = get_versions()['version']
-del get_versions
