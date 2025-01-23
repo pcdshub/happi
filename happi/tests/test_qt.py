5c54a5604a862ca954f236a4e190c643f3ce1776
tangkong <tangkongrobert@gmail.com>
2025-01-23 11:32:01 -0800

    TST: add basic ui smoke tests

---
+++ happi/tests/test_qt.py
@@ -0,0 +1,24 @@
+import pytest
+from pytestqt.qtbot import QtBot
+
+from happi.client import Client
+
+try:
+    from happi.qt.widgets import HappiItemMetadataView, HappiSearchWidget
+    qt_missing = False
+except ImportError:
+    HappiSearchWidget = None
+    HappiItemMetadataView = None
+    qt_missing = True
+
+
+@pytest.mark.skipif(qt_missing, reason="qt packages not installed")
+def test_search_widget(qtbot: QtBot, mockjsonclient: Client):
+    search_widget = HappiSearchWidget(client=mockjsonclient)
+    qtbot.addWidget(search_widget)
+
+
+@pytest.mark.skipif(qt_missing, reason="qt packages not installed")
+def test_item_view(qtbot: QtBot, mockjsonclient: Client):
+    search_widget = HappiItemMetadataView(client=mockjsonclient)
+    qtbot.addWidget(search_widget)
